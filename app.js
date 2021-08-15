2fee77a0adc3127c7f03c68d239c597010487cb9
elianarojas <elianabellarojas@gmail.com>
2021-08-14 23:31:35 -0300

    Subir funciones que vinculan los textarea superior e inferior con el meme

--- app.js
+++ app.js
@@ -3,6 +3,12 @@
 const inputURL = document.getElementById("input-url");
 const imagenDelMeme = document.querySelector("img");
 const contenedorImgMeme = document.getElementById("contenedor-img-meme");
+
+const textoSuperiorTextarea = document.getElementById("texto-superior-textarea");
+const textoInferiorTextarea = document.getElementById("texto-inferior-textarea");
+const textoSuperiorMeme = document.getElementById("texto-sup-meme");
+const textoInferiorMeme = document.getElementById("texto-inf-meme");
+
 // URL de la imagen asociada a la sección del meme
 inputURL.onkeyup = (event) => {
     
@@ -11,8 +17,20 @@ inputURL.onkeyup = (event) => {
     const inputUrlValue = inputURL.value;
     imagenDelMeme.src=inputUrlValue;
 
-    // imagenDelMeme.style.width = "100%";
-    // imagenDelMeme.style.height = "100%";
-    
-
 }
+
+// Escribir texto en la parte superior del recuadro del meme
+
+textoSuperiorTextarea.addEventListener('keyup' , () => {
+
+    const valorTextareaSup = textoSuperiorTextarea.value;
+    textoSuperiorMeme.innerText = valorTextareaSup;
+});
+
+// Escribir texto en la parte inferior del recuadro del meme
+
+textoInferiorTextarea.addEventListener('keyup' , () => {
+
+    const valorTextareaInf = textoInferiorTextarea.value;
+    textoInferiorMeme.innerText = valorTextareaInf;
+});
